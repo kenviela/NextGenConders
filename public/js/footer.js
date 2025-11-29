a05ab5d10643b800e18e62ec39ea23cdda76e628
Daniela <danielaguzman50@outlook.es>
2025-11-29 11:12:36 -0500

    actualizacion de header y footer

---
+++ public/js/footer.js
@@ -0,0 +1,8 @@
+// llamar al footer
+fetch("/components/Footer/Footer.html")
+  .then((res) => res.text())
+  .then((html) => {
+    const container = document.createElement("div");
+    container.innerHTML = html;
+    document.body.appendChild(container);
+  });
---
+++ public/js/header.js
@@ -0,0 +1,6 @@
+// Cargar el header a la vista
+fetch("/components/Header/Header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  });
